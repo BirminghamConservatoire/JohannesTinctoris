d2f113e3ad0ed76a78511e989d668b13f31657cd
Anna Plaksin <Anna.Plaksin@bcu.ac.uk>
2020-08-07 11:37:04 +0200

    Loading files is still awkward

--- Tinctoris/Music/getMusic.js
+++ Tinctoris/Music/getMusic.js
@@ -7,11 +7,56 @@ var musicCode;
 editable = false;
 
 $(document).ready(function() {
-    $("#load").click(function() {
-        grabFromFile(this.getAttribute("load"));
+    
+    function grabMusic(text) 
+    {
+        if(text)
+        {
+            musicCode = text;
+            console.log(text);
+            return text;
+        }
+    }
+
+    function grabFromFile(fileUri)
+    {
+        var client = new XMLHttpRequest();
+        client.onreadystatechange = function() {
+            grabMusic(client.responseText);
+        };
+        
+        client.open('GET', fileUri);
+        client.send();
+    }
+
+    function makeMusicDoc()
+    {
+        doc.draw();
+    }
+
+
+    $(".load").click(function() {
         if(musicCode)
         {
+            doc = new MusicHolder(musicCode, document.getElementById("content"));
             makeMusicDoc();
+        }
+        else
+        {
+            grabFromFile(this.getAttribute("id"));
+            musicCode = "";
+        }
+    });
+
+    $(".text").click(function() {
+        if(musicCode)
+        {
+            $("#content").text(musicCode);
+            musicCode = "";
+        }
+        else
+        {
+            grabFromFile(this.getAttribute("id"));
         }
     });
 
@@ -19,33 +64,5 @@ $(document).ready(function() {
         $("#content").empty();
     });
 });
-
-function grabMusic(text) 
-{
-    if(text)
-    {
-        musicCode = text;
-        console.log(text);
-        return text;
-    }
-}
-
-function grabFromFile(fileUri)
-{
-    var resp = "";
-    var client = new XMLHttpRequest();
-    client.onreadystatechange = function() {
-        grabMusic(client.responseText);
-    };
-    
-    client.open('GET', fileUri);
-    client.send();
-}
-
-function makeMusicDoc()
-{
-    doc = new MusicHolder(musicCode, document.getElementById("content"));
-    doc.draw();
-}
 
 

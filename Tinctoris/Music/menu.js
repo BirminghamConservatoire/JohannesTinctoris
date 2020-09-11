7990a23499a8dd209bfa3c318876de3e4c86bfeb
Anna Plaksin <Anna.Plaksin@bcu.ac.uk>
2020-09-11 13:16:14 +0200

    Add first 4 Tinctoris Masses

--- Tinctoris/Music/menu.js
+++ Tinctoris/Music/menu.js
@@ -1,36 +1,154 @@
-/** @fileoverview Contains the menu structure for the music edition site */
+{/** @fileoverview Contains the menu structure of the sidebar menu for the music edition site */
 
 var musicMenu = {
-    "tinctoris" :
+    "tMasses" :
     {
         0 : 
         {
-            "name" : "Minimal",
-            "class" : "load",
-            "id" : "music/minimal.txt"
+            "name" : "Missa L'homme armé",
+            "class" : "none",
+            "parts": 
+            {
+                0 :
+                {
+                    "name" : "Kyrie",
+                    "class" : "load",
+                    "id" : "music/1 Missa L’homme armé/1a Missa L’homme armé - Kyrie.txt"
+                },
+                1 :
+                {
+                    "name" : "Gloria",
+                    "class" : "load",
+                    "id" : "music/1 Missa L’homme armé/1b Missa L’homme armé - Gloria.txt"
+                },
+                2 :
+                {
+                    "name" : "Credo",
+                    "class" : "load",
+                    "id" : "music/1 Missa L’homme armé/1c Missa L’homme armé - Credo.txt"
+                },
+                3 :
+                {
+                    "name" : "Sanctus",
+                    "class" : "load",
+                    "id" : "music/1 Missa L’homme armé/1d Missa L’homme armé - Sanctus.txt"
+                },
+                4 :
+                {
+                    "name" : "Agnus Dei",
+                    "class" : "load",
+                    "id" : "music/1 Missa L’homme armé/1e Missa L’homme armé - Agnus.txt"
+                }
+            }
         },
         1 : 
-        {   "name" : "Minimal text",
-            "class" : "text",
-            "id" : "music/minimal.txt"
+        {   "name" : "Missa primi toni",
+            "class" : "none",
+            "parts" : 
+            {
+                0 : 
+                {
+                    "name" : "Gloria",
+                    "class" : "load",
+                    "id" : "music/2 Missa primi toni/2a Missa primi toni - Gloria.txt"
+                },
+                1 :   
+                {
+                    "name" : "Credo",
+                    "class" : "load",
+                    "id" : "music/2 Missa primi toni/2b Missa primi toni - Credo.txt"
+                },
+                2 :
+                {
+                    "name" : "Sanctus",
+                    "class" : "load",
+                    "id" : "music/2 Missa primi toni/2c Missa primi toni - Sanctus.txt"
+                }
+            }
+        },
+        2 :
+        {
+            "name" : "Missa secundi toni irregularis",
+            "class" : "none",
+            "parts": 
+            {
+                0 :
+                {
+                    "name" : "Kyrie",
+                    "class" : "load",
+                    "id" : "music/3 Missa secundi toni irregularis/3a Missa secundi toni irregularis - Kyrie.txt"
+                },
+                1 :
+                {
+                    "name" : "Gloria",
+                    "class" : "load",
+                    "id" : "music/3 Missa secundi toni irregularis/3b Missa secundi toni irregularis - Gloria.txt"
+                },
+                2 :
+                {
+                    "name" : "Credo",
+                    "class" : "load",
+                    "id" : "music/3 Missa secundi toni irregularis/3c Missa secundi toni irregularis - Credo.txt"
+                },
+                3 :
+                {
+                    "name" : "Sanctus",
+                    "class" : "load",
+                    "id" : "music/3 Missa secundi toni irregularis/3d Missa secundi toni irregularis - Sanctus.txt"
+                },
+                4 :
+                {
+                    "name" : "Agnus Dei",
+                    "class" : "load",
+                    "id" : "music/3 Missa secundi toni irregularis/3e Missa secundi toni irregularis - Agnus.txt"
+                }
+            }
+        },
+        3 :
+        {
+            "name" : "Missa quinti toni irregularis",
+            "class" : "none",
+            "parts": 
+            {
+                0 :
+                {
+                    "name" : "Kyrie",
+                    "class" : "load",
+                    "id" : "music/4 Missa quinti toni irregularis/4a Missa quinti toni irregularis - Kyrie.txt"
+                },
+                1 :
+                {
+                    "name" : "Gloria",
+                    "class" : "load",
+                    "id" : "music/4 Missa quinti toni irregularis/4b Missa quinti toni irregularis - Gloria.txt"
+                },
+                2 :
+                {
+                    "name" : "Credo",
+                    "class" : "load",
+                    "id" : "music/4 Missa quinti toni irregularis/4c Missa quinti toni irregularis - Credo.txt"
+                },
+                3 :
+                {
+                    "name" : "Sanctus",
+                    "class" : "load",
+                    "id" : "music/4 Missa quinti toni irregularis/4d Missa quinti toni irregularis - Sanctus & Benedictus.txt"
+                },
+                4 :
+                {
+                    "name" : "Agnus Dei",
+                    "class" : "load",
+                    "id" : "music/4 Missa quinti toni irregularis/4e Missa quinti toni irregularis - Agnus Dei.txt"
+                }
+            }
         }
     },
     "dufay" :
     {
-        0 :
-        {
-            "name" : "Second",
-            "class" : "load",
-            "id" : "music/second.txt"
-        }
+
     },
     "busnois" : 
     {
-        0 :
-        {   
-            "name" : "Third",
-            "class" : "load",
-            "id" : "music/variantIssues.txt"
-        }
+        
     }
-};
+}};

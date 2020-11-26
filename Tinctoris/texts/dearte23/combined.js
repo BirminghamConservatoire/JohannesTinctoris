b8d47177d9536db246f0987b87f75dd76e9556c8
Anna Plaksin <Anna.Plaksin@bcu.ac.uk>
2020-11-26 11:10:29 +0100

    Added Liber de Arte Contrapuncti II & III to page menu

---
+++ Tinctoris/texts/dearte23/combined.js
@@ -0,0 +1,26 @@
+texts.dearte23 = {};
+//if(!texts.treatises) texts.treatises = [];
+//texts.treatises.push("dearte23");
+texts.dearte23.sources = [];
+texts.dearte23.edited = 'De arte contrapuncti II - III\n\
+Editor: Jeffrey J. Dean\n\
+Checked by:\n\
+Date established: 25 November 2020\n\
+Base file: De arte contrapuncti II (BU).txt\n\
+Sources:\n\
+V	E-VAu 835, fols. 116r–144r (modern numeration)\n\
+BU	I-Bu 2573, fols. 133r–166r (modern numeration)\n\
+Br1	B-Br II 4147 Mus., fols. 80ra–101ra\n\
+Seg	E-SE s.s., fol. CC.iiij r (II.xxiii.ex2)\n\
+Per	I-PEc 1013, fols. 81r–89r, 107r–116r <italic>passim</italic> (II.xx.ex2, xxii.exx1–4, xxiii.exx1–2, xxv.ex1, xxvi.ex1, xxviii.ex1)\n\
+Per1	I-PEc 1013, fol. 83v\n\
+Per2	I-PEc 1013, fol. 87v\n\
+Bo2	I-Bc A 71, pp. 286–98 (modern numeration; II.xx.ex2, xxii.exx1–4, xxiii.exx1–2, xxv.ex1, xxvi.ex1)\n\
+Tr88	I-TRbc 1375 [88], fols. 403r, 414v–415r (modern numeration; II.xxx.ex1, xxxiv.ex1)\n\
+Luc	I-La 238, fol. 14r (modern numeration; II.xxx.ex1)\n\
+ModC	I-MOe α.M.1.13, fol. 120r (modern numeration; II.xxx.ex1)\n\
+CS14	V-CVbav Cappella Sistina 14, fol. 40r (modern numeration; II.xxx.ex1)\n\
+Col	E-Sc 5.1.43, fol. o9v [107v] (II.xxx.ex2)\n\
+MC	I-MC 871, p. 11 [267] (III.iii.ex2)\n\
+CanC	<citation>Canti C nº cento Cinquanta</citation> (Venice: Ottaviano Petrucci, 10.ii.1503 [n.s. 1504, RISM 1504^1^]), opening 1018s [118s] (II.xxx.ex2)\n\
+<treatise>[We shall add Books II and III as soon as we can.]</treatise>';

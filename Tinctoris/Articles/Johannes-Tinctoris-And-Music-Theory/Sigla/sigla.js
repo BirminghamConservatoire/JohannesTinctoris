d7317ef87a75857bc9298c650535c6d1752c747e
David Lewis <david.lewis@oerc.ox.ac.uk>
2020-12-02 11:14:56 +0000

    Remove redundant code, support for sigla.json as array rather than object

--- Tinctoris/Articles/Johannes-Tinctoris-And-Music-Theory/Sigla/sigla.js
+++ Tinctoris/Articles/Johannes-Tinctoris-And-Music-Theory/Sigla/sigla.js
@@ -1,7 +1,3 @@
-const sigla = {
-	EVAu835: ["<em>E-VAu</em> 835", 'Valencia, Universitat de València, Biblioteca Histórica, MS 835 [<em>olim</em> 844]	<a href="http://weblioteca.uv.es/cgi/view.pl?source=uv_ms_0835">http://weblioteca.uv.es/cgi/view.pl?source=uv_ms_0835</a>']
-}
-
 function showCode(el){
 	var sig = el.parentNode;
 	$(sig).children(".code").show();
@@ -15,36 +11,23 @@ function showDetails(el){
 function toggleDetails(el){
 	$(el).children(".details").toggle();
 }
-/*
-function replaceSiglum(i, siglum){
-	var siglumCode = siglum.innerHTML;
-	if(siglumCode in sigla) {
-		console.log(Object.keys(sigla));
-		siglum.innerHTML = '<span onclick="toggleDetails(this)" class="siglumInfo"><span class="code" tabindex="0">'+sigla[siglumCode][0]
-			+'</span><span  tabindex="0" class="details">'+sigla[siglumCode][1]+'</span></span>';
-	}
-}
-function replaceSigla(){
-	$(".siglum").each(replaceSiglum);
-	$(".siglum .details").hide();
-}
-*/
 function replaceSiglum(sigla, i, siglum){
-	console.log(sigla, i, siglum);
+	// Given a siglum (in a <span>) in plain text in an article,
+	// replace with an active link from sigla
 	var siglumCode = siglum.innerHTML;
-	if(siglumCode in sigla) {
-		siglum.innerHTML = '<span onclick="toggleDetails(this)" class="siglumInfo"><span class="code" tabindex="0">'+sigla[siglumCode].siglum
-			+'</span><span  tabindex="0" class="details">'+sigla[siglumCode].fullName+'</span></span>';
+	var siglumMatch = sigla.find(sigObj => sigObj.id == siglumCode);
+	if(siglumMatch) {
+		siglum.innerHTML = '<span onclick="toggleDetails(this)" class="siglumInfo"><span class="code" tabindex="0">'+siglumMatch.siglum
+			+'</span><span  tabindex="0" class="details">'+siglumMatch.fullName+'</span></span>';
 	}
 }
 function replaceSigla(sigla){
-	console.log(sigla)
+	// Replace all sigla spans in article with
+	// expandable links
 	var rS = replaceSiglum.bind(null, sigla);
 	$(".siglum").each(rS);
 	$(".siglum .details").hide();
 }
-
-
 $(function() {
 	$.get('/Tinctoris/Articles/Johannes-Tinctoris-And-Music-Theory/Sigla/sigla.json', replaceSigla, 'json')
 });

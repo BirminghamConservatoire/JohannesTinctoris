b1cae93eebdba1677e322e8967e7d975dadfa9c9
Anna Plaksin <Anna.Plaksin@bcu.ac.uk>
2021-10-14 16:31:14 +0200

    Add dynamic subemenu and fetching of editions

--- Tinctoris/Music/viewMusic.js
+++ Tinctoris/Music/viewMusic.js
@@ -7,8 +7,15 @@ var baseUrl = currentUrl.origin + currentUrl.pathname;
 /** @var currentParams parameters of current url */
 var currentParams = currentUrl.searchParams;
 
+/** @var {string} currentFile contains the content of the currently loaded file */
+var currentFile;
+/** @var {MusicHolder} currentMusic currently displayed MusicHolder */
 var currentMusic;
 
+/**
+ * Fetches a JT:CPW text file from the given URL and draws it.
+ * @param {string} fileUrl 
+ */
 function fetchMusic(fileUrl)
 {
     if(fileUrl)
@@ -18,14 +25,81 @@ function fetchMusic(fileUrl)
             return response.text();
         })
         .then(function(text) {
+            currentFile = text;
             currentMusic = new MusicHolder(text, document.getElementById("content"));
+            currentMusic.draw();
         });
     }
 }
 
-function fillSubMenu(itemID)
+/**
+ * Fills the submenu below the navbar with sections or pieces to navigate.
+ * @param {Object} item 
+ * @param {string} activeID 
+ */
+function fillSubMenu(item, activeID)
 {
-    
+    /** 
+     * <li class="nav-item">
+     *    <a class="nav-link active" href="#">item.label</a>
+     * </li>
+    */
+
+    for(let entry of item["parts"])
+    {
+        currentParams.set("file", entry["file"]);
+        currentParams.set("item", entry["id"]);
+        currentUrl.search = currentParams.toString();
+        let entryHref = currentUrl.toString();
+        $("#submenu").append("<li class='nav-item'><a id='"
+            +entry["id"]+
+            "' class='nav-link' href='"
+            +entryHref+"'>"
+            +entry["label"]+"</a></li>");
+        if(entry["id"]===activeID)
+        {
+            $("#"+entry["id"]).addClass("active");
+        }
+    }
+}
+
+/**
+ * Retrieves the item from the menu object by the item id.
+ * @param {string} itemID 
+ * @param {Object} itemList 
+ * @returns {Object}
+ */
+function findMenuItem(itemID, itemList)
+{
+    var menuItem;
+
+    for(let item of itemList)
+    {
+        if(item["id"]===itemID)
+        {
+            menuItem = item;
+            break;
+        }
+        else if(item["items"]!=null)
+        {
+            menuItem = findMenuItem(itemID, item["items"]);
+            if(menuItem!=null)
+            {
+                break;
+            }
+        }
+        else if(item["parts"]!=null)
+        {
+            let partItem = findMenuItem(itemID, item["parts"]);
+            if(partItem!=null)
+            {
+                menuItem = item;
+                break;
+            }
+        }
+    }
+
+    return menuItem;
 }
 
 $(document).ready(function() {
@@ -33,6 +107,14 @@ $(document).ready(function() {
     var fileUrl = currentParams.get("file");
     var currentItemID = currentParams.get("item");
 
+    var currentMenuItem = findMenuItem(currentItemID, menu);
+
+    if(currentMenuItem["parts"])
+    {
+        fillSubMenu(currentMenuItem, currentItemID);
+    }
+
+    fetchMusic(fileUrl);
 
     $(window).resize(function() {
         wrapWidth = $(".content").width();
